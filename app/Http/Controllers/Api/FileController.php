d3dbc74e498742b5608089a3f1299b4e260f4a19
NoahSavant <xayvier01@gmail.com>
2023-11-18 23:51:59 +0700

    TH-6 update upload file clound

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -2,36 +2,26 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Constants\AuthenConstant\StatusResponse;
 use App\Http\Controllers\Controller;
 use App\Models\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 class FileController extends Controller
 {
     public function upload(Request $request)
     {
-        $file = $request->file('file');
+        $uploadedFile = $request->file('file');
 
-        // Get the original extension of the file
-        $extension = $file->getClientOriginalExtension();
+        $result = Cloudinary::upload($uploadedFile->getRealPath(), []);
 
-        // Generate a unique filename to avoid conflicts
-        $fileName = uniqid() . '.' . $extension;
+        $url = $result['secure_url'];
 
-        // Store the file with the generated filename and the 'public' disk
-        $path = $file->storeAs('public', $fileName);
+        return response()->json([
+            'url' => $url
+        ], StatusResponse::SUCCESS);
 
-        // Create a record in the database with the original filename and the stored path
-        $newFile = File::create([
-            'filename' => $file->getClientOriginalName(),
-            'path' => $path,
-        ]);
-
-        // Generate the URL based on the stored path
-        $url = env('APP_URL') . Storage::url($path);
-
-        return response()->json(['url' => $url]);
     }
 }
