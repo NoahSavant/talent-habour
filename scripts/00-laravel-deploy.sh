e4d06fb40075daf5546184b860dac2f66425fb7b
NoahSavant <xayvier01@gmail.com>
2023-11-19 18:30:54 +0700

    update project v3

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 echo 'Running composer'
 composer install --no-dev --working-dir=/var/www/html
- 
+php artisan cache:clear 
+
 echo 'Caching config...'
 php artisan config:cache
 
@@ -10,12 +11,11 @@ php artisan route:cache
  
 echo 'Running migrations...'
 php artisan migrate --force
-
-echo 'Running storage...'
 php artisan cache:clear
 
+echo 'Running storage...'
 php artisan storage:link
+php artisan cache:clear
 
 echo 'Running queue...'
-php artisan queue:restart
-php artisan queue:work &
+php artisan queue:work & php artisan cache:clear
